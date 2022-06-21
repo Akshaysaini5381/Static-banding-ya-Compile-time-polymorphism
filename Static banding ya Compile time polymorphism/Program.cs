735c73c7b29fac16f9f33cd2d39b8428266a2a8a
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-22 01:38:31 +0530

    Add project files.

---
+++ Static banding ya Compile time polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Static_banding_ya_Compile_time_polymorphism
+{
+    class MyClass
+    {
+        public  void show()
+        {
+            Console.WriteLine("Hello :");
+        }
+    }
+
+
+    class Program : MyClass
+    {
+        public new  void show()
+        {
+            Console.WriteLine("Hii :");
+        }
+        static void Main(string[] args)
+        {
+            MyClass obj = new Program();
+            obj.show();
+        }
+    }
+
+}
